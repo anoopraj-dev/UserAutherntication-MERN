6ed9ffcf9bb34a60068951adde1f9066b10eba7d
Anoop Raj <anoopraj.dev@gmail.com>
2025-08-20 17:03:50 +0530

    added userModel and test routes

--- api/app.js
+++ api/app.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 
-
+app.use('/user',userRouter)
 
 export default app;
---
+++ api/routes/userRoutes.js
@@ -0,0 +1,9 @@
+
+import express from 'express';
+import { testApi } from '../controllers/user.controller.js';
+
+const router = express.Router();
+
+router.get('/',testApi)
+
+export default router;
