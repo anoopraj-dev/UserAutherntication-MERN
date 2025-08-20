1ff069e5363db75ade44ed16235bbbf8ae7479bd
Anoop Raj <anoopraj.dev@gmail.com>
2025-08-20 14:32:01 +0530

    user pages added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/user/Home'
+import SignIn from './pages/user/SignIn'
+import Profile from './pages/user/Profile'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-7xl'>hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/signin' element={<SignIn/>} />
+        <Route path='/profile' element={<Profile/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
